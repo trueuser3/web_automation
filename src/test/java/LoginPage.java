74a2c5a4480e4afafd6fb1f1d56fe6b3d781f6b6
Egor <57009978+trueuser3@users.noreply.github.com>
2023-12-06 04:04:24 +0300

    Update and rename LoginPage.java to LoginPage.java

---
+++ src/test/java/LoginPage.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class LoginPage {
+
+    final SelenideElement USER_NAME = $x("//input[@name=\"st.email\"]");
+    final SelenideElement PASSWORD_NAME = $x("//input[@name=\"st.password\"]");
+    final SelenideElement SUBMIT_BUTTON = $x("//input[@value=\"Log in to OK\"]");
+    final SelenideElement INFO_ABOUT_ERROR = $x("//div[@class=\"input-e login_error\"]");
+
+    public void openWebsite(String url) {
+        Selenide.open(url);
+    }
+
+    public void inputLogin(String login) {
+        $(USER_NAME).shouldBe(Condition.visible);
+        USER_NAME.click();
+        USER_NAME.sendKeys(login);
+    }
+
+    public void inputPassword(String password) {
+        $(PASSWORD_NAME).shouldBe(Condition.visible);
+        PASSWORD_NAME.click();
+        PASSWORD_NAME.sendKeys(password);
+    }
+
+    public void submitLoginAndPassword() {
+        $(SUBMIT_BUTTON).shouldBe(Condition.visible);
+        SUBMIT_BUTTON.click();
+    }
+
+    public String emptyInputLoginAndPassword() {
+        $(INFO_ABOUT_ERROR).shouldBe(Condition.visible);
+        return INFO_ABOUT_ERROR.getText();
+    }
+}
