48c3db11640aefbd2788ab8a7dd74617c848a56f
Egor <57009978+trueuser3@users.noreply.github.com>
2023-12-10 06:34:47 +0300

    Update LoginPage.java

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -10,6 +10,10 @@ public class LoginPage {
     final SelenideElement PASSWORD_NAME = $x("//input[@name=\"st.password\"]");
     final SelenideElement SUBMIT_BUTTON = $x("//input[@value=\"Log in to OK\"]");
     final SelenideElement INFO_ABOUT_ERROR = $x("//div[@class=\"input-e login_error\"]");
+    final SelenideElement SEARCH_BUTTON = $x("//input[@class = \"input__prt1l __size-m__prt1l input__mofy2 input-field__on39s __right-icon__on39s __white-toolbar__on39s __redesigned__on39s\"]");
+    final SelenideElement INTERESTS_BUTTON = $x("//a[@href = \"/hobby\"]");
+    final SelenideElement PEOPLE_BUTTON = $x("//a[@href = \"/search/profiles\"]");
+
 
     public void openWebsite(String url) {
         Selenide.open(url);
@@ -36,4 +40,19 @@ public class LoginPage {
         $(INFO_ABOUT_ERROR).shouldBe(Condition.visible);
         return INFO_ABOUT_ERROR.getText();
     }
+
+    public void clickOnSearchButtonInterests() {
+        $(SEARCH_BUTTON).shouldBe(Condition.visible);
+        SEARCH_BUTTON.click();
+        $(INTERESTS_BUTTON).shouldBe(Condition.visible);
+        INTERESTS_BUTTON.click();
+    }
+
+    public void clickOnSearchButtonPeople() {
+        $(SEARCH_BUTTON).shouldBe(Condition.visible);
+        SEARCH_BUTTON.click();
+        $(PEOPLE_BUTTON).shouldBe(Condition.visible);
+        PEOPLE_BUTTON.click();
+    }
+
 }
