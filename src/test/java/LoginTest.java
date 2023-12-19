890c9273a1543873a2a1c63fc851f935ab718718
Egor <57009978+trueuser3@users.noreply.github.com>
2023-12-20 02:00:29 +0300

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,11 +1,12 @@
-import com.codeborne.selenide.Configuration;
+import Pages.LoginPage;
+import Pages.ProfilePage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+
 public class LoginTest extends BaseTest {
-    final String BASE_URL = "https://ok.ru/";
     final String CORRECT_LOGIN = "QAbot1";
     final String CORRECT_PASSWORD = "3ml3ZdbnK9";
     final String INCORRECT_LOGIN = "ExampleLogin";
@@ -20,9 +21,7 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void checkLoginSucces() {
-        Configuration.holdBrowserOpen = true;
         LoginPage loginPage = new LoginPage();
-        loginPage.openWebsite(BASE_URL);
         loginPage.inputLogin(CORRECT_LOGIN);
         loginPage.inputPassword(CORRECT_PASSWORD);
         loginPage.submitLoginAndPassword();
@@ -34,9 +33,7 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void checkNoPassword() {
-        Configuration.holdBrowserOpen = true;
         LoginPage loginPage = new LoginPage();
-        loginPage.openWebsite(BASE_URL);
         loginPage.inputLogin(CORRECT_LOGIN);
         loginPage.inputPassword("");
         loginPage.submitLoginAndPassword();
@@ -48,9 +45,7 @@ public class LoginTest extends BaseTest {
     @ParameterizedTest
     @ValueSource(strings = {"", INCORRECT_PASSWORD})
     void checkEmptyLogin(String word) {
-        Configuration.holdBrowserOpen = true;
         LoginPage loginPage = new LoginPage();
-        loginPage.openWebsite(BASE_URL);
         loginPage.inputLogin("");
         loginPage.inputPassword(word);
         loginPage.submitLoginAndPassword();
@@ -62,9 +57,7 @@ public class LoginTest extends BaseTest {
     @ParameterizedTest
     @ValueSource(strings = {INCORRECT_LOGIN, UPPERCASE_LOGIN})
     void checkIncorrectLogin(String word) {
-        Configuration.holdBrowserOpen = true;
         LoginPage loginPage = new LoginPage();
-        loginPage.openWebsite(BASE_URL);
         loginPage.inputLogin(word);
         loginPage.inputPassword(CORRECT_PASSWORD);
         loginPage.submitLoginAndPassword();
@@ -76,35 +69,13 @@ public class LoginTest extends BaseTest {
     @ParameterizedTest
     @ValueSource(strings = {INCORRECT_PASSWORD, UPPERCASE_PASSWORD})
     void checkIncorrectPassword(String word) {
-        Configuration.holdBrowserOpen = true;
         LoginPage loginPage = new LoginPage();
-        loginPage.openWebsite(BASE_URL);
         loginPage.inputLogin(CORRECT_LOGIN);
         loginPage.inputPassword(word);
         loginPage.submitLoginAndPassword();
         String text_of_error = loginPage.emptyInputLoginAndPassword();
         Assertions.assertEquals(text_of_error, INCORRECT_LOGIN_OR_PASSWORD);
 
-    }
-
-    @Test
-    public void checkSearchButtonInterests() {
-        Configuration.holdBrowserOpen = true;
-        LoginPage loginPage = new LoginPage();
-        loginPage.openWebsite(BASE_URL);
-        loginPage.clickOnSearchButtonInterests();
-        InterestsPage interestsPage = new InterestsPage();
-        interestsPage.visibleHeadTitle();
-    }
-
-    @Test
-    public void checkSearchButtonPeople() {
-        Configuration.holdBrowserOpen = true;
-        LoginPage loginPage = new LoginPage();
-        loginPage.openWebsite(BASE_URL);
-        loginPage.clickOnSearchButtonPeople();
-        PeoplePage peoplePage = new PeoplePage();
-        peoplePage.visiblePeopleFoundCounter();
     }
 
 }
