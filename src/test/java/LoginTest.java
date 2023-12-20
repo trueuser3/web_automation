a3cc9fe458df899ab545f16da88e216b022741a0
Egor <57009978+trueuser3@users.noreply.github.com>
2023-12-20 05:09:45 +0300

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,34 +1,26 @@
 import Pages.LoginPage;
 import Pages.ProfilePage;
+import model.UserModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 
 public class LoginTest extends BaseTest {
-    final String CORRECT_LOGIN = "QAbot1";
-    final String CORRECT_PASSWORD = "3ml3ZdbnK9";
-    final String INCORRECT_LOGIN = "ExampleLogin";
-    final String INCORRECT_PASSWORD = "ExamplePassword";
-    final String UPPERCASE_LOGIN = "QABOT1";
-    final String UPPERCASE_PASSWORD = "3ML3ZDBNK9";
-    final String TOP_MENU = "Верхнее меню";
-    final String ENTER_YOUR_USERNAME = "Enter your username";
-    final String ENTER_PASSWORD = "Enter password";
-    final String INCORRECT_LOGIN_OR_PASSWORD = "Incorrect username and/or password";
-
-
-    @Test
-    public void checkLoginSucces() {
-        LoginPage loginPage = new LoginPage();
-        loginPage.inputLogin(CORRECT_LOGIN);
-        loginPage.inputPassword(CORRECT_PASSWORD);
-        loginPage.submitLoginAndPassword();
-        ProfilePage profilePage = new ProfilePage();
-        String href = profilePage.getHrefProfilePage();
-        Assertions.assertEquals(href, TOP_MENU);
-    }
+    static final String CORRECT_LOGIN = "QAbot1";
+    static final String CORRECT_PASSWORD = "3ml3ZdbnK9";
+    static final String INCORRECT_LOGIN = "ExampleLogin";
+    static final String INCORRECT_PASSWORD = "ExamplePassword";
+    static final String UPPERCASE_LOGIN = "QABOT1";
+    static final String UPPERCASE_PASSWORD = "3ML3ZDBNK9";
+    static final String TOP_MENU = "Верхнее меню";
+    static final String ENTER_YOUR_USERNAME = "Enter your username";
+    static final String ENTER_PASSWORD = "Enter password";
+    static final String INCORRECT_LOGIN_OR_PASSWORD = "Incorrect username and/or password";
 
 
     @Test
@@ -54,28 +46,42 @@ public class LoginTest extends BaseTest {
 
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {INCORRECT_LOGIN, UPPERCASE_LOGIN})
-    void checkIncorrectLogin(String word) {
-        LoginPage loginPage = new LoginPage();
-        loginPage.inputLogin(word);
-        loginPage.inputPassword(CORRECT_PASSWORD);
-        loginPage.submitLoginAndPassword();
-        String text_of_error = loginPage.emptyInputLoginAndPassword();
-        Assertions.assertEquals(text_of_error, INCORRECT_LOGIN_OR_PASSWORD);
 
+    @ParameterizedTest
+    @MethodSource(value = "testLoginsAndPasswords")
+    public void succesLoginAndPassword(UserModel userModel) {
+        LoginPage loginPage = new LoginPage();
+        loginPage.inputLogin(userModel.getLoginName());
+        loginPage.inputPassword(userModel.getPassword());
+        loginPage.submitLoginAndPassword();
+        ProfilePage profilePage = new ProfilePage();
+        String href = profilePage.getHrefProfilePage();
+        Assertions.assertEquals(href, TOP_MENU);
+    }
+
+    private static Stream<UserModel> testLoginsAndPasswords() {
+        UserModel goodLoginAndPassword = UserModel.builder().loginName(CORRECT_LOGIN).password(CORRECT_PASSWORD).build();
+        UserModel uppercaseLogin = UserModel.builder().loginName(UPPERCASE_LOGIN).password(CORRECT_PASSWORD).build();
+        return Stream.of(goodLoginAndPassword, uppercaseLogin);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {INCORRECT_PASSWORD, UPPERCASE_PASSWORD})
-    void checkIncorrectPassword(String word) {
+    @MethodSource(value = "testLoginsAndPasswords2")
+    public void nonSuccesLoginOrPassword(UserModel userModel) {
         LoginPage loginPage = new LoginPage();
-        loginPage.inputLogin(CORRECT_LOGIN);
-        loginPage.inputPassword(word);
+        loginPage.inputLogin(userModel.getLoginName());
+        loginPage.inputPassword(userModel.getPassword());
         loginPage.submitLoginAndPassword();
         String text_of_error = loginPage.emptyInputLoginAndPassword();
         Assertions.assertEquals(text_of_error, INCORRECT_LOGIN_OR_PASSWORD);
+    }
 
+    private static Stream<UserModel> testLoginsAndPasswords2() {
+        UserModel user1 = UserModel.builder().loginName(INCORRECT_LOGIN).password(CORRECT_PASSWORD).build();
+        UserModel user2 = UserModel.builder().loginName(CORRECT_LOGIN).password(INCORRECT_PASSWORD).build();
+        UserModel user3 = UserModel.builder().loginName(CORRECT_LOGIN).password(UPPERCASE_PASSWORD).build();
+        UserModel user4 = UserModel.builder().loginName(INCORRECT_LOGIN).password(INCORRECT_PASSWORD).build();
+        return Stream.of(user1, user2, user3, user4);
     }
 
 }
