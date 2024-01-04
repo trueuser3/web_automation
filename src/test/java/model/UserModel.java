5ff32a7e15261cd0076fa2f0a92622969a8e84c5
Egor <57009978+trueuser3@users.noreply.github.com>
2024-01-05 01:38:41 +0300

    Create UserModel.java

---
+++ src/test/java/model/UserModel.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserModel {
+    private String loginName;
+    private String password;
+}
