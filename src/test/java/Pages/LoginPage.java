c6ba20c256899638c8dcf2bc490e4b9c14420e71
Egor <57009978+trueuser3@users.noreply.github.com>
2024-01-15 13:46:31 +0300

    Update LoginPage.java

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -12,27 +12,26 @@ public class LoginPage {
     final SelenideElement SUBMIT_BUTTON = $x("//input[@value=\"Log in to OK\"]");
     final SelenideElement INFO_ABOUT_ERROR = $x("//div[@class=\"input-e login_error\"]");
 
-
-    public void inputLogin(String login) {
-        USER_NAME.shouldBe(Condition.visible);
-        USER_NAME.click();
+    public LoginPage inputLogin(String login) {
+        LoginPage tempLoginPage = new LoginPage();
+        USER_NAME.shouldBe(Condition.visible).click();
         USER_NAME.sendKeys(login);
+        return tempLoginPage;
     }
 
-    public void inputPassword(String password) {
-        PASSWORD_NAME.shouldBe(Condition.visible);
-        PASSWORD_NAME.click();
+    public LoginPage inputPassword(String password) {
+        LoginPage tempLoginPage = new LoginPage();
+        PASSWORD_NAME.shouldBe(Condition.visible).click();
         PASSWORD_NAME.sendKeys(password);
+        return tempLoginPage;
     }
 
     public void submitLoginAndPassword() {
-        SUBMIT_BUTTON.shouldBe(Condition.visible);
-        SUBMIT_BUTTON.click();
+        SUBMIT_BUTTON.shouldBe(Condition.visible).click();
     }
 
     public String emptyInputLoginAndPassword() {
-        INFO_ABOUT_ERROR.shouldBe(Condition.visible);
-        return INFO_ABOUT_ERROR.getText();
+        return INFO_ABOUT_ERROR.shouldBe(Condition.visible).getText();
     }
 
 }
